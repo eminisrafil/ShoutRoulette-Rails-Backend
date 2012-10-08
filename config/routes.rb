68e8fb27dc79a39e6e8dc9c89f85fc8f9a30a2ff
Jeff Escalante <hello@jenius.me>
2012-10-07 21:28:38 -0400

    about page and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ ShoutrouletteV2::Application.routes.draw do
 
   root to: "shouts#index"
   get '/room/:id/:position' => "rooms#show", as: 'show_room'
+  get '/about' => "pages#about", as: 'about'
 
 end
