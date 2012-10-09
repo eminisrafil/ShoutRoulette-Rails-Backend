fa689a78c7d6a9218b12d50ef0d478109ec1a565
Jeff Escalante <hello@jenius.me>
2012-10-09 00:01:55 -0400

    add topics route and controller

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,7 @@
+class TopicsController < ApplicationController
+
+  def new
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 ShoutrouletteV2::Application.routes.draw do
 
+  # these routes need tons of work. but later
+
   root to: "shouts#index"
   get 'room/:id/:position' => "rooms#show", as: 'show_room'
   get 'about' => "pages#about", as: 'about'
   get 'bunnies' => "admin#index", as: 'admin'
   delete 'topic/:id/remove' => "admin#remove_topic", as: 'remove_topic'
   post 'close' => "rooms#close"
-  post 'newtopic' => "topics#newtopic", as: 'new_topic'
+  post 'topics/new' => "topics#new", as: 'new_topic'
+
 end
