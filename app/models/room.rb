8431d5af78361afc02d42d907b816ea8e34067ce
Jeff Escalante <hello@jenius.me>
2012-10-08 22:48:54 -0400

    refactored room close method

--- app/models/room.rb
+++ app/models/room.rb
@@ -17,11 +17,9 @@ class Room < ActiveRecord::Base
     selected_room
   end
 
-  def self.close(params) ##removes user from seat, updates user_session to not in room
-    room = Room.where("id = ?", params[:id]).first
+  def close(params) # removes user from seat, updates user_session to not in room
     position = params[:position] == 'agree' ? "position_2" : "position_1"
-    room.update_attribute(position, nil)
-    room
+    update_attribute(position, nil)
   end
 
   def observe(topic, params)
