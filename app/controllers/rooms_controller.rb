1a335395195f17a8794e5023f009fe445a7d8f41
Jeff Escalante <hello@jenius.me>
2012-10-09 15:47:10 -0400

    add popular topics to shout page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
 
   def show
+    @topics = Topic.top_popular
     @topic = Topic.find(params[:id])
     @room = Room.create_or_join(@topic, params, request)
     @position = params[:position] == 'agree' ? "position_2" : "position_1"
