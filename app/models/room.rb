608754308413aa33c7fb9d9fd926b6c90437c02d
Jeff Escalante <hello@jenius.me>
2012-10-08 21:11:53 -0400

    clean up first method

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,21 +1,25 @@
 class Room < ActiveRecord::Base
   belongs_to :topic
   attr_accessible :session_id, :position_1, :position_2, :closed
-	API_KEY = '20193772'                # should be a string
-	API_SECRET = 'cf90dfef4da7ba913239ee34e3d0a387411cd2e5' 	           # should be a string
-	OTSDK = OpenTok::OpenTokSDK.new API_KEY, API_SECRET
+
+	OTSDK = OpenTok::OpenTokSDK.new '20193772', 'cf90dfef4da7ba913239ee34e3d0a387411cd2e5'
+
   def create_or_join(params)
-	position = params[:position] == 'agree' ? "position_2" : "position_1"
+
+    @topic = Topic.find(params[:id])
+    position = params[:position] == 'agree' ? "position_2" : "position_1"
   	selected_room = Room.where("#{position} is null").shuffle.first
+
     if !selected_room
     	session = generate_session(params[:ip_address]).to_s
-      selected_room = Topic.find(params[:id]).rooms.create({ session_id: session })
+      selected_room = @topic.rooms.create({ session_id: session })
       selected_room[position] = generate_publisher(session)
       selected_room.save!
     else
       selected_room.update_attribute(position, generate_publisher(selected_room.session_id))
     end
-    return selected_room
+
+    selected_room
   end
 
   def generate_session(request)
