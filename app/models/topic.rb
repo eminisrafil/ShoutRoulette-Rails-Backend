ad44c2f5feeecfd6c22e11eab1782e01f79cbd87
Steve <keepevets@gmail.com>
2012-10-09 10:16:14 -0400

    changed timestamp settings

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,14 +7,14 @@ class Topic < ActiveRecord::Base
   def self.top_popular
   	pop_count = 5
     time_check = Time.now.to_formatted_s(:number).to_i - 600
-  	Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > '#{time_check}' then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title, rooms.topic_id ORDER BY room_count desc LIMIT 5")
+  	Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > (CURRENT_TIMESTAMP - interval '10' minutes) then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title, rooms.topic_id ORDER BY room_count desc LIMIT 5")
   end
 
   def self.sort_all(params)
     time_check = Time.now.to_formatted_s(:number).to_i - 600
     case params[:sort]
     	when 'popular'
-    		topics = Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > '#{time_check}' then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title ORDER BY room_count desc")
+    		topics = Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > (CURRENT_TIMESTAMP - interval '10' minutes) then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title ORDER BY room_count desc")
     	when 'debaters'
 
     	when 'observers'
@@ -22,7 +22,7 @@ class Topic < ActiveRecord::Base
     else ##default most recent
 
     end
-    topics = Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > '#{time_check}' then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title ORDER BY topics.id desc")
+    topics = Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' and last_checked > (CURRENT_TIMESTAMP - interval '10' minutes) then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' and last_checked > '#{time_check}' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title ORDER BY topics.id desc")
   end
 
   def self.page(params)
