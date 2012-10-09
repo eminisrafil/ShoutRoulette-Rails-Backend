9b3c7610209918c1d047da4ca7bb8412ac8a3054
Jeff Escalante <hello@jenius.me>
2012-10-09 17:01:54 -0400

    get rid of don't click, add get angry page, style it a bit more

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ ShoutrouletteV2::Application.routes.draw do
 
   root to: "shouts#index"
   get 'room/:id/:position' => "rooms#show", as: 'show_room'
-  get 'about' => "pages#about", as: 'about'
+
+  scope 'pages', controller: 'pages' do
+    get :about, :get_angry
+  end
+
   get 'bunnies' => "admin#index", as: 'admin'
   delete 'topic/:id/remove' => "admin#remove_topic", as: 'remove_topic'
   post 'close' => "rooms#close"
