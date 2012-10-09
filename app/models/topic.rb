190d8e75657459c6a5815e9f81dc6d7330e80769
Steve <keepevets@gmail.com>
2012-10-09 07:54:04 -0400

    attempt at fixing pg sql error

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,13 +6,13 @@ class Topic < ActiveRecord::Base
 
   def self.top_popular
   	pop_count = 5
-  	Topic.joins('left join `rooms` on `rooms`.`topic_id` = `topics`.`id`').group('`topics`.`id`').order('count(`rooms`.`id`) desc').limit(pop_count)
+  	Topic.joins('left join `rooms` on `rooms`.`topic_id` = `topics`.`id`').group('`topics`.`id`').order('count(`rooms`.*) desc').limit(pop_count)
   end
 
   def self.sort_all(params)
     case params[:sort]
     	when 'popular'
-    		topics = Topic.joins('left join `rooms` on `rooms`.`topic_id` = `topics`.`id`').group('`topics`.`id`').order('count(`rooms`.`id`) desc')
+    		topics = Topic.joins('left join `rooms` on `rooms`.`topic_id` = `topics`.`id`').group('`topics`.`id`').order('count(`rooms`.*) desc')
     	when 'debaters'
 
     	when 'observers'
