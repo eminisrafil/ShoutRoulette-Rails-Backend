67a3a0d660e599d2b7f3a267eed23c6a6505e4f3
Steve <keepevets@gmail.com>
2012-10-09 01:15:13 -0400

    fixed room matchups

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,9 +7,8 @@ class Room < ActiveRecord::Base
       selected_room = Room.observe(topic)
     else
       position = params[:position] == 'agree' ? "position_2" : "position_1"
-    	selected_room = Room.where("(? is null or ? = '') and topic_id = ? and closed = '0'", position, topic.id).shuffle.first
+    	selected_room = Room.where("#{position} is null and topic_id = '#{topic.id}' and (closed = '0' or closed is null)").shuffle.first
       session = !selected_room ? OTSDK.createSession.to_s : selected_room.session_id
-      
       if !selected_room
         selected_room = topic.rooms.create({ session_id: session, :"#{position}" => publisher_token(session) })
       else
