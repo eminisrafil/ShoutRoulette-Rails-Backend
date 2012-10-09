3bb758cdc82f76d141452db640ce5cacad664690
Steve <keepevets@gmail.com>
2012-10-09 10:58:33 -0400

    fixed remaining glitch from user_sess last checked attempt

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -3,13 +3,12 @@ class UserSession < ActiveRecord::Base
   attr_accessible :topic_id, :ip_address, :observing, :last_checked, :token_id, :room_id, :session_id
 
   def self.log_user(request, room, params)
-    time_now = Time.now.to_formatted_s(:number).to_i
   	observe = params[:position] == 'observe' ? true : false
   	sess = UserSession.find(:first, :conditions=>{:session_id => request.session_options[:id].to_s})
   	if !sess
-  		UserSession.create({topic_id: room.topic_id, room_id: room.id, session_id: request.session_options[:id].to_s, observing: observe, last_checked: time_now.to_s.to_i})
+  		UserSession.create({topic_id: room.topic_id, room_id: room.id, session_id: request.session_options[:id].to_s, observing: observe})
   	else
-  		sess.update_attributes({topic_id: room.topic_id, room_id: room.id, observing: observe, last_checked: time_now.to_s.to_i})
+  		sess.update_attributes({topic_id: room.topic_id, room_id: room.id, observing: observe})
   	end
   end
   def close_sess(request)
