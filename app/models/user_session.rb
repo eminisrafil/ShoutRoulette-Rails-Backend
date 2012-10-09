c4edcecb588a71b8e57b268a9580ea389909482d
Steve <keepevets@gmail.com>
2012-10-09 07:46:53 -0400

    remove user sess to fix heroku

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -6,9 +6,9 @@ class UserSession < ActiveRecord::Base
   	observe = params[:position] == 'observe' ? true : false
   	sess = UserSession.find(:first, :conditions=>{:ip_address => request.session_options[:id].to_s})
   	if !sess
-  		UserSession.create({'topic_ic': topic.id, 'ip_address': request.session_options[:id].to_s, 'observing': observe})
+  		#UserSession.create({'topic_ic': topic.id, 'ip_address': request.session_options[:id].to_s, 'observing': observe})
   	else
-  		sess.update_attributes('topic_id', topic.id, 'observing': observe)
+  		#sess.update_attributes('topic_id', topic.id, 'observing': observe)
   	end
   end
 end
