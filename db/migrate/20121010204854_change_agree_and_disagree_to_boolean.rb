b51f7fefba4494b564863ed7564af3e2b2e6a9fa
Jeff Escalante <hello@jenius.me>
2012-10-10 17:47:56 -0400

    change migration

--- db/migrate/20121010204854_change_agree_and_disagree_to_boolean.rb
+++ db/migrate/20121010204854_change_agree_and_disagree_to_boolean.rb
@@ -1,6 +1,8 @@
 class ChangeAgreeAndDisagreeToBoolean < ActiveRecord::Migration
   def change
-    change_column :rooms, :agree, :boolean
-    change_column :rooms, :disagree, :boolean
+    remove_column :rooms, :agree
+    remove_column :rooms, :disagree
+    add_column :rooms, :agree, :boolean
+    add_column :rooms, :disagree, :boolean
   end
 end
