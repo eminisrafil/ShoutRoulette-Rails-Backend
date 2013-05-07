266305c0eefbda28c2305de5d0aa2e9763bb097b
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-07 16:43:03 -0400

    test json

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,7 +4,7 @@ class ShoutsController < ApplicationController
     @topics = Topic.top_popular
     @all = Topic.sort_all(params)
     respond_to do |format|
-  	  format.html # show.html.erb
+  	  #format.html # show.html.erb
   	  format.json { render :json => { 'Topics' => @all} }
 	  end
   end
