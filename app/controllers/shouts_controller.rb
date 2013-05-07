3c5338ae2854447147af9827b31f0905fc7980bf
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-07 16:36:19 -0400

    extra space

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -3,7 +3,6 @@ class ShoutsController < ApplicationController
   def index
     @topics = Topic.top_popular
     @all = Topic.sort_all(params)
-
     respond_to do |format|
   	  format.html # show.html.erb
   	  format.json { render :json => { 'Topics' => @all} }
