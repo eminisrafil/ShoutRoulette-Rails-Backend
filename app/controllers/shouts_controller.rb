9617dd4d32875e56c21149acb8fdc9b6feb9b920
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-07 19:11:47 -0400

    fix html rendering

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,8 +4,9 @@ class ShoutsController < ApplicationController
     @topics = Topic.top_popular
     @all = Topic.sort_all(params)
     respond_to do |format|
-  	  #format.html # show.html.erb
-  	  format.json { render :json => { 'Topics' => @all} }
+    	format.json { render :json => { 'Topics' => @all} }
+  	  format.html # show.html.erb
+  	 
 	  end
   end
   
