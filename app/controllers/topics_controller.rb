2884077a79eeea542c07e45edee0c0924abd2e43
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-30 18:31:04 -0400

    Topic/new responds to JSON

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,13 @@
 class TopicsController < ApplicationController
 
   def new
-    Topic.create({ title: params[:topic] })
-    redirect_to root_path
+    newTopic = Topic.create({ title: params[:topic] })
+    respond_to do |format|
+    	#format.json { render :json => { 'Topics' => @all.to_json(methods:['agree_debaters'])}}
+      format.json { render :json => { 'Topics' => newTopic}}
+  	  format.html redirect_to root_path
+	  end
+    
   end
 
 end
