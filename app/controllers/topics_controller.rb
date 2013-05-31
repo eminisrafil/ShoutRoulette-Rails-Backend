0c6b604022c11871dfa63c580178c4d451144157
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-30 22:14:33 -0400

    Topic/new responds to JSON fix

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,7 +5,7 @@ class TopicsController < ApplicationController
     respond_to do |format|
     	#format.json { render :json => { 'Topics' => @all.to_json(methods:['agree_debaters'])}}
       format.json { render :json => { 'Topics' => newTopic}}
-  	  format.html redirect_to root_path
+  	  format.html { redirect_to root_path}
 	  end
     
   end
