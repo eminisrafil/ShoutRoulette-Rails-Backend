b6f753dea3a3966b76b6986a45d38b5ca8836b37
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-30 23:25:35 -0400

    Topic/new responds to JSON fix

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,17 +1,16 @@
 class TopicsController < ApplicationController
-
+	before_filter :check_params
   def new
     newTopic = Topic.create({ title: params[:topic] })
-    puts params.inspect
-    puts Rails.logger.debug params.inspect
-    puts debug(params)
-    puts "just created new topic\n\n"
+
     respond_to do |format|
     	#format.json { render :json => { 'Topics' => @all.to_json(methods:['agree_debaters'])}}
       format.json { render :json => { 'Topics' => newTopic}}
   	  format.html { redirect_to root_path}
 	  end
-    
+  end
+  def check_params
+  	redirect_to root_path unless params[:topic].length >3 && params[:topic].length <145
   end
 
 end
