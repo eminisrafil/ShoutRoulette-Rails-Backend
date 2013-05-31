7af610a793e901d0b5edaa24175e1b66ceb70c5a
mt-sagarmatha <eminisrafil@yahoo.com>
2013-05-30 22:42:28 -0400

    Topic/new responds to JSON fix

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,8 @@ class TopicsController < ApplicationController
 
   def new
     newTopic = Topic.create({ title: params[:topic] })
-    puts params
+    puts params.inspect
+    puts "just created new topic\n\n"
     respond_to do |format|
     	#format.json { render :json => { 'Topics' => @all.to_json(methods:['agree_debaters'])}}
       format.json { render :json => { 'Topics' => newTopic}}
