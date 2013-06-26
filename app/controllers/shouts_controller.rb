a3f3a3aca605b4ad6e5edc3d8a6a5062ca8df867
mt-sagarmatha <eminisrafil@yahoo.com>
2013-06-26 19:45:42 -0400

    add offset to topic results

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,12 +4,11 @@ class ShoutsController < ApplicationController
   def index
     @topics = Topic.top_popular
     @all = Topic.sort_all(params)
-    
-    #puts "END => @TOPICS:           #{@topics} \n\n\n"
-    #puts "END => @ALL:           #{@all} \n\n\n"
+    @sorted = Topic.sort_limited
+
+
     respond_to do |format|
-    	#format.json { render :json => { 'Topics' => @all.to_json(methods:['agree_debaters'])}}
-      format.json { render :json => { 'Topics' => @all}, :methods => [:agree_debaters, :disagree_debaters, :observers]}
+      format.json { render :json => { 'Topics' => @sorted}, :methods => [:agree_debaters, :disagree_debaters, :observers]}
   	  format.html # show.html.erb
 	  end
   end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -20,19 +20,20 @@ class Topic < ActiveRecord::Base
     Topic.all.each do |topic|
       total = topic.debaters + topic.observers
       poptop << [topic, total]
-      #puts "topic:           #{topic} \n\n\n"
-     #puts "poptop to_json:   #{poptop.to_json}\n\n\n"
     end
     #sort
     poptop.sort! { |a,b| a[1] <=> b[1] }
     
     #return only top 3 topics, strip out total values
     poptop.collect { |a| a[0] }.reverse[0..3]
-     #puts "END => poptop:           #{poptop} \n\n\n"
-     #puts "END => poptop to_json:   #{poptop.to_json}\n\n\n"
 
   	# Topic.find_by_sql("SELECT topics.id, topics.title, sum(case when user_sessions.observing = '1' then 1 else 0 end) as observer_count, sum(case when user_sessions.observing = '0' then 1 else 0 end) as debater_count, count(rooms.id) as room_count FROM topics left join rooms on rooms.topic_id = topics.id left join user_sessions on (user_sessions.topic_id = topics.id and user_sessions.room_id = rooms.id) GROUP BY topics.id, topics.title, rooms.topic_id ORDER BY room_count desc LIMIT 5")
   end
+
+  def self.sort_limited (limit = 3, offset = 0)
+    sorted = Topic.all.sort_by{|x| x.debaters}.reverse[offset..(limit+offset)]
+    #sorted
+  end 
 
   def self.sort_all(params)
     topics = Topic.all.reverse
