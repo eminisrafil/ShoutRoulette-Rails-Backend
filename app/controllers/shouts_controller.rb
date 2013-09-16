30fadb64343865b9018c7c2a28f77b113e28a4fd
mt-sagarmatha <eminisrafil@yahoo.com>
2013-09-16 13:48:30 -0400

    current page for pagination

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -2,15 +2,17 @@ class ShoutsController < ApplicationController
   require 'will_paginate/array'
   
   def index
-    @topics = Topic.top_popular
-    @all = Topic.sort_all(params).paginate(:page => params[:page], :per_page => 20)
-    @sorted = Topic.sort_limited.paginate(:page => params[:page], :per_page => 20)
+    current_page = params[:page]
 
-    @all.to_json
+    @topics = Topic.top_popular
+    @all = Topic.sort_all(params).paginate(:page => current_page, :per_page => 20)
+    @sorted = Topic.sort_limited.paginate(:page => current_page, :per_page => 20)
+
+    puts @all.to_json
 
 #'Pagination' => ['total_pages' => @all.total_pages, 'current_page' => 20, 'page' => params[:page]]
     respond_to do |format|
-      format.json { render :json => { 'Topics' => @all, 'Pagination' =>  ['total_pages' => @all.total_pages, 'current_page' => params[:page], 'per_page' => 20]}, :methods => [:agree_debaters, :disagree_debaters, :observers]}
+      format.json { render :json => { 'Topics' => @all, 'Pagination' =>  ['total_pages' => @all.total_pages, 'current_page' => current_page, 'per_page' => 20]}, :methods => [:agree_debaters, :disagree_debaters, :observers]}
   	  format.html # show.html.erb
 	  end
   end
