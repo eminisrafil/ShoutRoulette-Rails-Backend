b0be16fa54d41cb79921f098f225af18df6ee87b
mt-sagarmatha <eminisrafil@yahoo.com>
2013-10-22 16:27:57 -0400

    invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,6 +3,7 @@ class InvitesController < ApplicationController
   	puts "###User Agent is coming up###"
     puts request.user_agent
     @path = "/room/#{params[:room_id]}/#{params[:room_position]}?sessionId=#{params[:sessionId]}"
+    #check ios version: http://stackoverflow.com/questions/18905686/itunes-review-url-and-ios-7-ask-user-to-rate-our-app-appstore-show-a-blank-pag
     if request.user_agent.match(/iPhone/i) #&& !request.user_agent.match(/ShoutRoulette/i)
       puts "This nigga has an iphone"
       render "/invites/index.html.erb"
