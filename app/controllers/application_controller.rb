51d079f312181fbc8c0831419b33d4e0f35d705f
eminisrafil <eminisrafil@gmail.com>
2013-11-26 18:01:29 -0500

    -put -> puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
 	def iOS_user_agent?
 		@user_agent ||= request.user_agent.try :match, /(iphone|ipod)/i 
-		puts "@user_agent: #{@user_agent}"
-		puts "request.user_agent: #{request.user_agent}" 
 	end
 
 	helper_method :iOS_user_agent?
