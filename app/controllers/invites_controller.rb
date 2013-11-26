6967fec579973464abce5c1996c3f1ddafe95ef6
eminisrafil <eminisrafil@gmail.com>
2013-11-26 17:57:43 -0500

    -put -> puts

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -6,7 +6,8 @@ class InvitesController < ApplicationController
     @path = "/room/#{room_id}/#{params[:room_position]}?q=#{params[:q]}"
     @topic_title = Room.find(room_id).topic.title
     @position = :room_position
-
+    puts "@topic_title #{@topic_title}"
+    puts "iOS_user_agent? : #{iOS_user_agent?}"
     #check ios version: http://stackoverflow.com/questions/18905686/itunes-review-url-and-ios-7-ask-user-to-rate-our-app-appstore-show-a-blank-pag
 
     if iOS_user_agent?
@@ -23,11 +24,13 @@ class InvitesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound do
     @topic_title = "Click here to start shouting!"
     puts "ACTIVE RECORD FAILED TO FIND THAT SHIT"
+    puts "iOS_user_agent? : #{iOS_user_agent?}"
     if iOS_user_agent?
       respond_to do |format|
         format.html { render "/invites/index.html.haml"}
       end
     else
+      puts "REDIRECTING TO @PATH #### #{iOS_user_agent?}" 
       redirect_to @path
     end
   end
