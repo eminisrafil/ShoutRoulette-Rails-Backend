2f14675db1edc4d9b1dd4afb829f0789043819b6
eminisrafil <eminisrafil@gmail.com>
2013-11-26 18:25:36 -0500

    -invites fixed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,5 @@ class ApplicationController < ActionController::Base
 	def iOS_user_agent?
 		@user_agent ||= request.user_agent.try :match, /(iphone|ipod)/i 
 	end
-
 	helper_method :iOS_user_agent?
 end
--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,35 +2,29 @@ class InvitesController < ApplicationController
   def index
   	puts "###User Agent is coming up###"
     puts request.user_agent
+    
     room_id = params[:room_id].to_i
     @path = "/room/#{room_id}/#{params[:room_position]}?q=#{params[:q]}"
     @topic_title = Room.find(room_id).topic.title
     @position = :room_position
-    puts "@topic_title #{@topic_title}"
-    puts "iOS_user_agent? : #{iOS_user_agent?}"
     #check ios version: http://stackoverflow.com/questions/18905686/itunes-review-url-and-ios-7-ask-user-to-rate-our-app-appstore-show-a-blank-pag
 
     if iOS_user_agent?
       respond_to do |format|
-        puts "rendering invites.html.haml!"
         format.html { render "/invites/index.html.haml"}
       end
     else
-      puts "REDIRECTING TO @PATH #### #{iOS_user_agent?}" 
       redirect_to @path
   	end
   end
 
   rescue_from ActiveRecord::RecordNotFound do
     @topic_title = "Click here to start shouting!"
-    puts "ACTIVE RECORD FAILED TO FIND THAT SHIT"
-    puts "iOS_user_agent? : #{iOS_user_agent?}"
     if iOS_user_agent?
       respond_to do |format|
         format.html { render "/invites/index.html.haml"}
       end
     else
-      puts "REDIRECTING TO @PATH #### #{iOS_user_agent?}" 
       redirect_to @path
     end
   end
